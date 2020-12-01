2e9d031e414b35205f5be3cee51515e07ad4328d
Andrey Alekseev <aletor2001@mail.ru>
2020-12-01 15:17:44 +0700

    Configure static files

--- events_memorizer/settings.py
+++ events_memorizer/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'events_memorizer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "events_memorizer/static"),
+]
+print(STATICFILES_DIRS)
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
