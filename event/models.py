337b6dd4600284c38e82ed8a7519051b719857a7
Andrey Alekseev <aletor2001@mail.ru>
2020-12-01 15:49:53 +0700

    Implement event model

--- event/models.py
+++ event/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from django.utils.translation import ugettext_lazy as _
+
+
 # Create your models here.
+
+class Event(models.Model):
+    """User event model.
+
+    Attributes:
+        owner (Manager): Foreign key User that created this event.
+        title (str): Event title.
+        place (bool): Location of the event.
+        description (set): Event description.
+
+    """
+    owner = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='own_events',
+        verbose_name=_('Owner'),
+    )
+
+    title = models.CharField(
+        max_length=100,
+        verbose_name=_('Title')
+    )
+
+    place = models.CharField(
+        max_length=250,
+        verbose_name=_('Place'),
+    )
+
+    description = models.TextField(
+        verbose_name=_('Description'),
+    )
