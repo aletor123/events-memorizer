2143eebc3b3a0a8b856fc4695c9947e420aacf96
Andrey Alekseev <aletor2001@mail.ru>
2020-12-03 02:17:59 +0700

    add event form to admin page

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,15 @@
+from django import forms
 from django.contrib import admin
 
 # Register your models here.
+from .forms import EventForm
+from .models import Event
+from easy_maps.models import Address
+
+
+class EventAdmin(admin.ModelAdmin):
+    form = EventForm
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Address)
