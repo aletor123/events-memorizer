389b9e6a17d32ee3c2a89c6fb24e9a5e7324887a
Andrey Alekseev <aletor2001@mail.ru>
2020-12-03 02:29:54 +0700

    Join sslserver
    This is necessary so that you can send https requests

--- events_memorizer/settings.py
+++ events_memorizer/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "sslserver",
     'easy_maps',
     'social_django',
     'account',
