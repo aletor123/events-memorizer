fcd1e6584951865641ef0fecf95821c5dca6314f
Andrey Alekseev <aletor2001@mail.ru>
2020-12-03 23:59:08 +0700

    Set MapWidget for place field

--- event/forms.py
+++ event/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from easy_maps.widgets import AddressWithMapWidget
 
 from .models import Event
+from .widgets import MapWidget
 
 
 class EventForm(forms.ModelForm):
@@ -9,5 +9,8 @@ class EventForm(forms.ModelForm):
         model = Event
         fields = ('title', 'place', 'description')
         widgets = {
-            'place': AddressWithMapWidget({'class': 'vTextField'})
+            'place': MapWidget(map_attrs={"center": [92.85656693081211, 56.01433428426304],}),
+        }
+        labels = {
+            'place': "Choose event's place",
         }
