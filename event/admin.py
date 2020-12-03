5d2803efa483e61286f490f54402afcf0ec9bf1a
Andrey Alekseev <aletor2001@mail.ru>
2020-12-04 00:27:15 +0700

    Add map to admin page

--- event/admin.py
+++ event/admin.py
@@ -2,14 +2,14 @@ from django import forms
 from django.contrib import admin
 
 # Register your models here.
+
 from .forms import EventForm
 from .models import Event
-from easy_maps.models import Address
+from mapbox_location_field.admin import MapAdmin
 
 
-class EventAdmin(admin.ModelAdmin):
+class EventAdmin(MapAdmin):
     form = EventForm
 
 
 admin.site.register(Event, EventAdmin)
-admin.site.register(Address)
