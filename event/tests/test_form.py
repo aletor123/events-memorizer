e27114972b92638fa278a938ad746cca24ccf8f1
Andrey Alekseev <aletor2001@mail.ru>
2020-12-04 14:21:58 +0700

    Create test for EventForm

---
+++ event/tests/test_form.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from event.forms import EventForm
+
+from event.widgets import MapWidget
+
+
+class EventFormTest(TestCase):
+    def test_map_widget_at_place_field_in_form(self):
+        form = EventForm
+        self.assertEqual(form.Meta.widgets['place'].__class__, MapWidget)
