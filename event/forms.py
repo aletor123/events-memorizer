9306351c3107f7edc1b3209d66eb30aec335c663
Andrey Alekseev <aletor2001@mail.ru>
2020-12-06 22:54:50 +0700

    Create form for admin page

--- event/forms.py
+++ event/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from .models import Event
-from .widgets import MapWidget
+from .widgets import MapWidget, AdminMapWidget
 
 
 class EventForm(forms.ModelForm):
@@ -10,6 +10,18 @@ class EventForm(forms.ModelForm):
         fields = ('title', 'point', 'place', 'description')
         widgets = {
             'point': MapWidget(map_attrs={"center": [92.85656693081211, 56.01433428426304], }),
+        }
+        labels = {
+            'point': "Choose event's place",
+        }
+
+
+class AdminEventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ('owner', 'title', 'point', 'place', 'description')
+        widgets = {
+            'point': AdminMapWidget(map_attrs={"center": [92.85656693081211, 56.01433428426304], }),
         }
         labels = {
             'point': "Choose event's place",
